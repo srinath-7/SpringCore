20316787224af31d66b1b846a9537fdd3573ea67
Srinath <gopathisrinath@gmail.com>
2020-07-10 09:49:53 +0530

    Setting up the Life cycle of bean using the xml Configuration

---
+++ SpringCore/src/main/java/com/sri/springcore/SpringCore/lifecycle/xmlConfig/Patient.java
@@ -0,0 +1,33 @@
+package com.sri.springcore.SpringCore.lifecycle.xmlConfig;
+
+public class Patient {
+	
+	 private int id;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		System.out.println("Inside the setter method");
+		this.id = id;
+	}
+	
+	public void hi() {
+		System.out.println("Inside hi method");
+	}
+	
+	public void bye() {
+		System.out.println("Bye method");
+	}
+
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + "]";
+	}
+	
+	
+	 
+	 
+
+}
