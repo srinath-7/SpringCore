ce55930b103e70718cf5a07e0964c80dd058aeba
Srinath <gopathisrinath@gmail.com>
2020-07-21 14:13:49 +0530

    Life cycle call backs using interfaces

--- SpringCore/src/main/java/com/sri/springcore/SpringCore/lifecycle/Interfaces/Patient.java
+++ SpringCore/src/main/java/com/sri/springcore/SpringCore/lifecycle/Interfaces/Patient.java
@@ -1,49 +1,53 @@
 package com.sri.springcore.SpringCore.lifecycle.Interfaces;
 
+
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 
 public class Patient implements InitializingBean, DisposableBean{
 
-		 private int id;
+	private int id;
 
-			public int getId() {
-				return id;
-			}
+	public int getId() {
+		return id;
+	}
 
-			public void setId(int id) {
-				System.out.println("Inside the setter method");
-				this.id = id;
-			}
-			
-			public void hi() {
-				System.out.println("Inside hi method");
-			}
-			
-			public void bye() {
-				System.out.println("Bye method");
-			}
+	public void setId(int id) {
+		System.out.println("Inside the setter method");
+		this.id = id;
+	}
+   
+	
+	public void hi() {
+		System.out.println("Inside hi method");
+	}
 
-			@Override
-			public String toString() {
-				return "Patient [id=" + id + "]";
-			}
-			
-			@Override
-			public void afterPropertiesSet() throws Exception {
-				// TODO Auto-generated method stub
-				System.out.println("Inside after propertiesset");
-				
-			}
-			
+	
+	public void bye() {
+		System.out.println("Bye method");
+	}
 
-			@Override
-			public void destroy() throws Exception {
-				// TODO Auto-generated method stub
-				System.out.println("Inside destroy method");
-				
-			}
-
+	@Override
+	public String toString() {
+		return "Patient [id=" + id + "]";
+	}
+	
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Inside the set method");
 		
 	}
 
+	@Override
+	public void destroy() throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("Inside the destroy method");
+	
+		
+	}
+
+	
+
+	
+}
